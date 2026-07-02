e3070a748b6237e4c05331ad06ead56f5d892b52
Marcelo Garuffi <marcelogaruffi@users.noreply.github.com>
2026-07-01 23:33:33 -0300

    Apply phone mask onChange instead of onBlur

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,23 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatPhone(value: string | null | undefined): string {
+  if (!value) return "";
+  let digits = value.replace(/\D/g, "");
+  if (digits.length <= 10) {
+    return digits.replace(/^(\d{2})(\d{0,4})(\d{0,4}).*/, (_, p1, p2, p3) => {
+      let res = `(${p1}`;
+      if (p2) res += `) ${p2}`;
+      if (p3) res += `-${p3}`;
+      return res;
+    });
+  } else {
+    return digits.replace(/^(\d{2})(\d{0,5})(\d{0,4}).*/, (_, p1, p2, p3) => {
+      let res = `(${p1}`;
+      if (p2) res += `) ${p2}`;
+      if (p3) res += `-${p3}`;
+      return res;
+    });
+  }
+}
